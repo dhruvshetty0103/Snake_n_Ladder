ac06cc994420c2f46147504f7734237f0bb356bf
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-19 19:08:07 +0530

    Added:Use case 1 with welcome message and player postion at 0 is starting of the problem statement

---
+++ Snake_n_Ladder/src/Snake_n_Ladder.java
@@ -0,0 +1,12 @@
+
+public class Snake_n_Ladder {
+
+	public static void main(String[] args) 
+	{
+		int IS_NO_PLAY = 1,IS_LADDER = 2,IS_SNAKE = 3,position=0;
+		int personPosition=0;
+		System.out.println("Welcome to Snake and Ladder game!");
+		System.out.println("Player postion at:"+personPosition);
+	}
+
+}
