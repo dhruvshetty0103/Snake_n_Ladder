de5a8e732c41a661a30c987744cc7e7c741d7586
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-19 20:31:16 +0530

    Modified:Code is modified where use case 3 and 4 is implemented with features like options of no play,ladder and snake until the player reaches 100

--- Snake_n_Ladder/src/Snake_n_Ladder.java
+++ Snake_n_Ladder/src/Snake_n_Ladder.java
@@ -2,8 +2,10 @@ import java.util.Random;
 
 public class Snake_n_Ladder {
 	
+	public static Random rand=new Random();
+	
 	public static int randInt(int min, int max) {
-	    Random rand=new Random();
+	    
 	    int randomNum = rand.nextInt((max - min) + 1) + min;
 
 	    return randomNum;
@@ -15,8 +17,32 @@ public class Snake_n_Ladder {
 		int personPosition=0,die_number=1;
 		System.out.println("Welcome to Snake and Ladder game!");
 		System.out.println("Player postion at:"+personPosition);
-		die_number=randInt(1,6);
-		System.out.println("Dice value:"+die_number);
+		
+		while(personPosition<=100)
+		{
+			int option=rand.nextInt((3-1)+1)+1;//choice
+			System.out.println(option);
+			if(option==1)
+				System.out.println("No play player at position:"+personPosition);
+			else if(option==2)
+			{	
+				die_number=randInt(1,6);
+				//if((personPosition+die_number)>100)
+					//personPosition=100;
+				//else
+					personPosition+=die_number;
+				System.out.println("Ladder:Player moves to position number:"+personPosition);
+			}
+			else
+			{
+				die_number=randInt(1,6);
+				//if((personPosition-die_number)<0)
+				//	personPosition=0;
+				//else
+					personPosition-=die_number;
+				System.out.println("Snake:Player moves to position number:"+personPosition);
+			}
+		}
 	}
 
 }
