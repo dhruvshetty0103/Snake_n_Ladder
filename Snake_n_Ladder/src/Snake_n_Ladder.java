1e1ace607927f87119688f7790031c4bb82f4117
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-19 19:43:25 +0530

    Modified:Code modified to generate random dice values.

--- Snake_n_Ladder/src/Snake_n_Ladder.java
+++ Snake_n_Ladder/src/Snake_n_Ladder.java
@@ -1,12 +1,22 @@
+import java.util.Random;
 
 public class Snake_n_Ladder {
+	
+	public static int randInt(int min, int max) {
+	    Random rand=new Random();
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
 
+	    return randomNum;
+	}
+	
 	public static void main(String[] args) 
 	{
-		int IS_NO_PLAY = 1,IS_LADDER = 2,IS_SNAKE = 3,position=0;
-		int personPosition=0;
+		//int IS_NO_PLAY = 1,IS_LADDER = 2,IS_SNAKE = 3,position=0;
+		int personPosition=0,die_number=1;
 		System.out.println("Welcome to Snake and Ladder game!");
 		System.out.println("Player postion at:"+personPosition);
+		die_number=randInt(1,6);
+		System.out.println("Dice value:"+die_number);
 	}
 
 }
