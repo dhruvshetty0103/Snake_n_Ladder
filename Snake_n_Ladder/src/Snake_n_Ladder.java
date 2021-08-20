3186505086997bafd4414a13e289bc45646186f6
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-20 19:25:05 +0530

    Modified and Refactored:Refactored the pervious code with commenting and rectifying some errors and modified the code according to use case 6

--- Snake_n_Ladder/src/Snake_n_Ladder.java
+++ Snake_n_Ladder/src/Snake_n_Ladder.java
@@ -14,12 +14,13 @@ public class Snake_n_Ladder {
 	public static void main(String[] args) 
 	{
 		//int IS_NO_PLAY = 1,IS_LADDER = 2,IS_SNAKE = 3,position=0;
-		int personPosition=0,die_number=1;
+		int personPosition=0,die_number=1,count=0; //here count is a variable to calculate the number of times the dice was rolled
 		System.out.println("Welcome to Snake and Ladder game!");
 		System.out.println("Player postion at:"+personPosition);
 		
 		do
 		{
+			count++;
 			int option=rand.nextInt((3-1)+1)+1;//choice
 			System.out.println("\nOption occurred:"+option);
 			if(option==1)
@@ -28,21 +29,26 @@ public class Snake_n_Ladder {
 			{	
 				die_number=randInt(1,6);
 				personPosition+=die_number;
-				if((personPosition+die_number)>99 && die_number!=1)
-					personPosition=personPosition-die_number;
-				
-				System.out.println("Ladder:Player moves to position number:"+personPosition+"--as dice number"+die_number);
+				if((personPosition)>100)//Condition to make sure that player wins when he gets exact dice no. to reach 100
+					{
+						personPosition=personPosition-die_number;
+						System.out.println("Ladder:But doesn't move as dice number is not exact to reach 100. as dice gave:"+die_number+" as player is at postion at:"+personPosition);
+					}
+				else
+					System.out.println("Ladder:Player moves to position number:"+personPosition+"--as dice number"+die_number);
 			}
 			else
 			{
 				die_number=randInt(1,6);
-				if((personPosition-die_number)<0)
+				if((personPosition-die_number)<0)//condition to check when the dice asks to move back below position 0 
 					personPosition=0;
 				else
 					personPosition-=die_number;
 				System.out.println("Snake:Player moves to position number:"+personPosition+"--as dice number"+die_number);
 			}
 		}while(personPosition<100);
+		
+		System.out.println("The dice was rolled:"+count+" times.");//Displaying the number of times dice rolled.
 	}
 
 }
